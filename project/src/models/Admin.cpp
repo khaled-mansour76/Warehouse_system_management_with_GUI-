d7d3de12a037e83616d6da71e856dce3e9d666f3
Khaled Mansour <khaledmansour0018@gmail.com>
2026-04-23 20:45:48 +0200

    Delete project/src/models directory

--- project/src/models/Admin.cpp
+++
@@ -1,3 +0,0 @@
-#include "Admin.h"
-
-Admin::Admin(std::string u, std::string p) : User(u, p, "admin") {}
--- project/src/models/Staff.cpp
+++
@@ -1,3 +0,0 @@
-#include "Staff.h"
-
-Staff::Staff(std::string u, std::string p) : User(u, p, "staff") {}
